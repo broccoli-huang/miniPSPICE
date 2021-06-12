713e6362945182b1f3322f510502c15fd2bad38a
Kevin <b09901175@ntu.edu.tw>
2021-06-13 00:58:46 +0800

    DCcal.py and DC2.txt updated.

--- DCcal.py
+++ DCcal.py
@@ -2,8 +2,6 @@ import numpy as np
 import copy
 
 def DCcalculator(NODE,SOURCE_pre,R_LST_pre,L_LST_pre,C_LST_pre):
-    n_left = copy.deepcopy(NODE)
-    n_removed = []
     R = []
     V = []
     N = copy.deepcopy(NODE)
@@ -13,16 +11,15 @@ def DCcalculator(NODE,SOURCE_pre,R_LST_pre,L_LST_pre,C_LST_pre):
     L_LST = [i if i[1]<i[2] else (i[0],i[2],i[1]) for i in L_LST_pre]
     C_LST = [i if i[1]<i[2] else (i[0],i[2],i[1]) for i in C_LST_pre]
 
-    for l in L_LST: #Constrain that l[1] < l[2]
+    for l in L_LST: #Constrain that l[1] < l[2].
         for r in R_LST:
-            if not (r[1] == l[1] and r[2] == l[2]): #R.remove(r)
-                if r[1] == l[2]:
+            if not (r[1] == l[1] and r[2] == l[2]): # in case l and r in parallel
+                if r[1] == l[2] and (r[0], l[1], r[2]) not in R:
                     R.append((r[0], l[1], r[2]))
-                elif r[2] == l[2]:
+                elif r[2] == l[2] and (r[0], r[1], l[1]) not in R:
                     R.append((r[0], r[1], l[1]))
-                else:
+                elif r not in R:
                     R.append(r)
-
         for v in SOURCE:
             if not (v[1] == l[1] and v[2] == l[2]):
                 if v[1] == l[2]:
@@ -33,14 +30,21 @@ def DCcalculator(NODE,SOURCE_pre,R_LST_pre,L_LST_pre,C_LST_pre):
                     V.append(v)
         for n in N:
             if n == l[2]: N.remove(n)
+    for l in L_LST:
+        for r in R:
+            if (r[1] == l[1] and r[2] == l[2]) or (r[1] == l[2] and r[2] == l[1]):
+                R.remove(r)
 
+    n_left = copy.deepcopy(N)
+    n_removed = []
+    
     for i in V:
         for j in i:
             for k in n_left:
                 if j == k:
                     n_removed.append(k)
                     n_left.remove(k)
-
+    
     NODE_V = np.zeros(len(NODE))
     node_v = np.zeros(len(N))
     NODE_I = np.zeros(len(N))
@@ -58,7 +62,7 @@ def DCcalculator(NODE,SOURCE_pre,R_LST_pre,L_LST_pre,C_LST_pre):
                         G[N.index(i)][N.index(j)] = -1/r[0]
                     elif r[1] == j and r[2] == i:
                         G[N.index(i)][N.index(j)] = -1/r[0]
-
+ 
     for i in n_removed:
         for j in n_removed:   #if n_removed.index(i) < n_removed.index(j):
             if n_removed.index(i) < n_removed.index(j):
@@ -73,7 +77,11 @@ def DCcalculator(NODE,SOURCE_pre,R_LST_pre,L_LST_pre,C_LST_pre):
                                 G[N.index(j)][N.index(r[2])] = -1/r[0]
 
     #print(G)
-    node_v = np.linalg.inv(G).dot(NODE_I.T).T
+    if np.linalg.det(G) != 0:
+        node_v = np.linalg.inv(G).dot(NODE_I.T).T
+    else:
+        node_v[0] = SOURCE[0][0]
+        node_v[1] = 0
 
     for l in L_LST:
         for node in N:
